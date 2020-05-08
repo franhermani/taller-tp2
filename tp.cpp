339748039e4c3027c21800e8f7a6b6516b24dd8c
franhermani <franhermani@gmail.com>
2020-05-08 18:06:56 -0300

    Loop for reading workers and map files

--- tp.cpp
+++ tp.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 #define OK 0
 #define ERROR 1
@@ -26,9 +28,33 @@ int process_input(const char *workers_path, const char *map_path) {
 }
 
 int process_workers_file(const char *workers_path) {
+    std::ifstream workers_file;
+    workers_file.open(workers_path, std::ifstream::in);
+    if (! workers_file.good()) {
+        std::cout << "Error al abrir el archivo de trabajadores\n";
+        return ERROR;
+    }
+    std::string line;
+    while (getline(workers_file, line)) {
+        // TODO: parsear linea y crear N trabajadores-threads
+    }
+    workers_file.close();
+
     return OK;
 }
 
 int process_map_file(const char *map_path) {
+    std::ifstream map_file;
+    map_file.open(map_path, std::ifstream::in);
+    if (! map_file.good()) {
+        std::cout << "Error al abrir el archivo de materias primas\n";
+        return ERROR;
+    }
+    char materia_prima;
+    while (map_file.get(materia_prima)) {
+        // TODO: enviar cada 'materia_prima' a su cola bloqueante
+    }
+    map_file.close();
+
     return OK;
 }
