4ff47dedde082211662f5c91fa49f2e1937f4371
franhermani <franhermani@gmail.com>
2020-05-07 23:49:49 -0300

    Main with some functions

--- tp.cpp
+++ tp.cpp
@@ -1,8 +1,34 @@
+#include <iostream>
 
 #define OK 0
 #define ERROR 1
 
-int main(int argc, char * const argv[]) {
+int process_input(const char *workers_path, const char *map_path);
+
+int process_workers_file(const char *workers_path);
+
+int process_map_file(const char *map_path);
+
+
+int main(int argc, char *argv[]) {
+    const char *workers_path = argv[1], *map_path = argv[2];
+
+    if (process_input(workers_path, map_path) == ERROR) return ERROR;
+
     return OK;
 }
 
+int process_input(const char *workers_path, const char *map_path) {
+    if (process_workers_file(workers_path) == ERROR) return ERROR;
+    if (process_map_file(map_path) == ERROR) return ERROR;
+
+    return OK;
+}
+
+int process_workers_file(const char *workers_path) {
+    return OK;
+}
+
+int process_map_file(const char *map_path) {
+    return OK;
+}
