163cbe8bef6312ded5e205ae56f10fc507ff5191
franhermani <franhermani@gmail.com>
2020-05-09 20:57:31 -0300

    Method to calculate points

--- AcumuladorPuntos.cpp
+++ AcumuladorPuntos.cpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <map>
 #include "AcumuladorPuntos.h"
 
 #define OK 0
@@ -18,7 +19,21 @@ int AcumuladorPuntos::sumarPuntos(std::vector<Recurso> recursos) {
 }
 
 int AcumuladorPuntos::calcularPuntos(std::vector<Recurso> recursos) {
-    return OK;
+    std::map<char, int> cantidades = {{'C', 0}, {'H', 0}, {'M', 0}, {'T', 0}};
+    int puntos = 0;
+
+    size_t i;
+    for (i = 0; i < recursos.size(); i ++)
+        cantidades[recursos[i].obtenerTipo()] += 1;
+
+    if (cantidades['T'] == 2 && cantidades['C'] == 1) {
+        puntos = 5;
+    } else if (cantidades['M'] == 3 && cantidades['H'] == 1) {
+        puntos = 2;
+    } else if (cantidades['C'] == 2 && cantidades['H'] == 2) {
+        puntos = 3;
+    }
+    return puntos;
 }
 
 int AcumuladorPuntos::obtenerPuntos() {
