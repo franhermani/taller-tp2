39e3e14f81961100427229e803ed95e5e204b3a7
franhermani <franhermani@gmail.com>
2020-05-12 22:02:15 -0300

    Added thread documentation

--- Thread.h
+++ Thread.h
@@ -5,14 +5,28 @@
 
 class Thread {
     std::thread thread;
+
 public:
+    // Constructor
     Thread();
+
+    // Inicializa el thread
     void start();
+
+    // Ejecuta el thread (virtual)
     virtual void run() = 0;
+
+    // Finaliza el thread
     void join();
+
+    // Destructor (virtual)
     virtual ~Thread();
+
+    // Constructor y asignacion por copia deshabilitados
     Thread(const Thread&) = delete;
     Thread& operator=(const Thread&) = delete;
+
+    // Constructor y asignacion por movimiento
     Thread(Thread&& other);
     Thread& operator=(Thread&& other);
 };
