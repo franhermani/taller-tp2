c6e310ac124b519dd0aa7c7a86b23eb1a4348fe1
franhermani <franhermani@gmail.com>
2020-05-14 16:40:30 -0300

    Added number of cmd params check

--- tp.cpp
+++ tp.cpp
@@ -1,10 +1,16 @@
 #include <string>
+#include <iostream>
 #include "Orchestrator.h"
 
 #define OK 0
 #define ERROR 1
+#define NUM_PARAMS 3
 
 int main(int argc, char *argv[]) {
+    if (argc != NUM_PARAMS) {
+        std::cout << "Uso: ./tp <archivo_trabajadores> <archivo_recursos>\n";
+        return ERROR;
+    }
     Orchestrator orchestrator;
     const std::string trabajadores_path = argv[1], recursos_path = argv[2];
 
