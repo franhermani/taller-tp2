c2bbdebbd76a2372d3bbaf4fc92b3f9dbff05170
franhermani <franhermani@gmail.com>
2020-05-15 21:08:57 -0300

    Deleted copy assignment

--- Recurso.h
+++ Recurso.h
@@ -8,6 +8,9 @@ public:
     // Recibe una referencia del tipo de recurso
     explicit Recurso(const char& tipo);
 
+    // Asignacion por copia deshabilitado
+    Recurso& operator=(const Recurso&) = delete;
+
     // Devuelve el tipo del recurso
     const char obtenerTipo() const;
 };
